f652c38dcfd608bcad442df462994b9eaf4cd886
peterwudi <peterwudi@gmail.com>
2013-10-20 18:21:43 -0400

    Timing alleviated by changing compilation effort
    Look into raw2rgb to see what the hell is going on. And what is the
    SDRAM used for

--- V/RAW2RGB.v
+++ V/RAW2RGB.v
@@ -112,15 +112,15 @@ begin
 		end
 		else if({iY_Cont[0],iX_Cont[0]}==2'b00)
 		begin
-			mCCD_R	<=	mDATAd_0;
-			mCCD_G	<=	mDATA_0+mDATAd_1;
-			mCCD_B	<=	mDATA_1;
+			mCCD_R	<=	mDATAd_1;
+			mCCD_G	<=	mDATA_1+mDATAd_0;
+			mCCD_B	<=	mDATA_0;
 		end
 		else if({iY_Cont[0],iX_Cont[0]}==2'b01)
 		begin
-			mCCD_R	<=	mDATA_0;
-			mCCD_G	<=	mDATAd_0+mDATA_1;
-			mCCD_B	<=	mDATAd_1;
+			mCCD_R	<=	mDATA_1;
+			mCCD_G	<=	mDATA_0+mDATAd_1;
+			mCCD_B	<=	mDATAd_0;
 		end
 	end
 end
